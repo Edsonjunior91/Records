0ab11b578a3a1c627e34ab4edc0c9b9702048c93
Edson Junior <edson_junior91@hotmail.com>
2020-06-30 00:46:31 -0300

    Records

---
+++ Records/src/Retangulo.java
@@ -0,0 +1,19 @@
+
+public record Retangulo(double largura, double altura) {
+
+	
+	public Retangulo(){
+		//this(4, 3);
+		this.largura = 4;
+		this.altura = 3;
+	}
+	public double calcularArea() {
+		return altura * largura;
+	}
+	
+	public static Retangulo create(double lado) {
+		return new Retangulo(lado, lado);
+	}
+	
+	
+}
